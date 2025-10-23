635f61da951482129fb4bd4dd6c9833b17c0bc23
msayers <qwa3wq@virginia.edu>
2025-10-23 19:24:22 -0400

    Line

--- Assets/Scripts/Board/PlayingArea.cs
+++ Assets/Scripts/Board/PlayingArea.cs
@@ -16,6 +16,7 @@ public class PlayingArea : MonoBehaviour
     private Card[] boardCards;
     
     
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
