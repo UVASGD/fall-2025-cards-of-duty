5f10b7266a5f929d24a57e2348fe40acb5c1c6ff
Aidan Liu <boomcreeper296@gmail.com>
2025-12-05 23:21:44 -0500

    Add title screen

--- Assets/Scripts/UI and Canvas Scripts/MainMenu.cs
+++ Assets/Scripts/UI and Canvas Scripts/MainMenu.cs
@@ -1,11 +1,19 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string gameplaySceneName;
     [SerializeField] private string creditsSceneName;
-
+    [SerializeField] private Canvas buttonCanvas;
+    [SerializeField] private TextMeshProUGUI pressAnyButtonText;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +25,34 @@ public class MainMenu : MonoBehaviour
         {
             Debug.LogError("MainMenu: Credits scene name is not set in the inspector!");
         }
+
+        StartCoroutine(FadeBackground());
+    }
+
+    IEnumerator FadeBackground()
+    {
+        Image rawImage = GetComponent<Image>();
+        Color originalColor = rawImage.color;
+        float fadeDuration = 2f;
+        float elapsedTime = 0f;
+        
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            rawImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
+        
+        pressAnyButtonText.gameObject.SetActive(true);
+        
+        InputSystem.onAnyButtonPress
+            .CallOnce(ctrl => ShowButtons());
+    }
+    void ShowButtons()
+    {
+        buttonCanvas.gameObject.SetActive(true);
+        pressAnyButtonText.gameObject.SetActive(false);
     }
 
     public void PlayGame()
